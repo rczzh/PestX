384bba0e2f775aab8dc041dc26f599b75bb18870
rczzh <ryanchng.0808@gmail.com>
2024-09-03 18:59:24 +0800

    Added Player Movement and Player Shoot

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerMovementSpeed;
+    public float playerShotSpeed;
+    public float playerFireRate;
+
+    public float lastFire;
+
+    new Rigidbody2D rigidbody;
+
+    public GameObject bulletPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        float shootHorizontal = Input.GetAxisRaw("ShootHorizontal");
+        float shootVertical = Input.GetAxisRaw("ShootVertical");
+
+        // player movement
+        rigidbody.velocity = new Vector3(horizontal * playerMovementSpeed, vertical * playerMovementSpeed, 0);
+
+        // player shooting
+        if ((shootHorizontal != 0 || shootVertical != 0) && Time.time > lastFire + playerFireRate)
+        {
+            Shoot(shootHorizontal, shootVertical);
+            lastFire = Time.time;
+        }
+    }
+
+    public void Shoot(float x, float y)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
+        bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector3(
+            (x < 0) ? Mathf.Floor(x) * playerShotSpeed : Mathf.Ceil(x) * playerShotSpeed,
+            (x < 0) ? Mathf.Floor(y) * playerShotSpeed : Mathf.Ceil(y) * playerShotSpeed,
+            0
+        ) ;
+    }
+}
