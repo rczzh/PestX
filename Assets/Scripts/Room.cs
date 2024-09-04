2752417fe659affb2bc0ca704677c9436f83395b
rczzh <ryanchng.0808@gmail.com>
2024-09-04 12:06:38 +0800

    Added tile map for starting area
    Just the starting area and not entire level

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public int Width;
+    public int Height;
+    public int X;
+    public int Y;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (RoomController.instance == null)
+        {
+            Debug.Log("You pressed play in the wrong scene");
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, new Vector3(Width, Height, 0));
+    }
+
+    public Vector3 GetRoomCentre()
+    {
+        return new Vector3(X * Width, Y * Height);
+    }
+}
